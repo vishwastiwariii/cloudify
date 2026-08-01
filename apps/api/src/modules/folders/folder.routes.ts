27d5dd34fa6e8551d138df7e42fbd5a9731853d0
vishwastiwariii <vishwastiwariii09@gmail.com>
2026-08-01 23:49:23 +0530

    feat: added get all file route

--- apps/api/src/modules/folders/folder.routes.ts
+++ apps/api/src/modules/folders/folder.routes.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express'
 import { getAllFolder, getFolder, handleCreateFolder, handleDeleteFolder, handleMoveFolder, handleUpdateFolder } from './folder.controller'
 import { authMiddleware } from '../../middleware/auth.middleware'
+import { getAllFile } from '../files/file.controller'
 
 const router: Router = express.Router()
 
@@ -10,5 +11,6 @@ router.get('/:folderId', authMiddleware, getFolder)
 router.post('/:folderId/move', authMiddleware, handleMoveFolder)
 router.delete('/:folderId', authMiddleware, handleDeleteFolder)
 router.patch('/:folderId', authMiddleware, handleUpdateFolder)
+router.get('/:folderId/files', authMiddleware, getAllFile)
 
 export default router
